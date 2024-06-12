8bc1b1698bd105c48cfc33443935fbae8fb675e2
pandyah5 <pandyahetav1@gmail.com>
2024-06-12 16:38:08 -0700

    Added a check for mask dimensions in batch normalization

--- keras/src/layers/normalization/batch_normalization.py
+++ keras/src/layers/normalization/batch_normalization.py
@@ -219,11 +219,10 @@ class BatchNormalization(Layer):
             if len(mask.shape) != len(inputs.shape) - 1:
                 # Raise a value error
                 raise ValueError(
-                    "The mask provided should be one dimension less "
-                    "than the inputs. Received: "
-                    f"mask.shape={mask.shape}, inputs.shape={inputs.shape}"
+                    "The mask provided should be one dimension less than the inputs. "
+                    f"Received: mask.shape={mask.shape}, inputs.shape={inputs.shape}"
                 )
-
+        
         input_dtype = standardize_dtype(inputs.dtype)
         if input_dtype in ("float16", "bfloat16"):
             # BN is prone to overflowing for float16/bfloat16 inputs, so we opt
